051f172ad209c1bf47982e119e2ecb9e893937af
AndrewBoshra <andrew.boshr@gmail.com>
2022-04-22 19:30:04 +0200

    implemented textures

--- Phase2/source/common/texture/texture-utils.cpp
+++ Phase2/source/common/texture/texture-utils.cpp
@@ -33,9 +33,31 @@ our::Texture2D* our::texture_utils::loadImage(const std::string& filename, bool 
     }
     // Create a texture
     our::Texture2D* texture = new our::Texture2D();
-    //Bind the texture such that we upload the image data to its storage
     //TODO: (Req 4) Finish this function to fill the texture with the data found in "pixels" and generate the mipmaps if requested
+    //Bind the texture such that we upload the image data to its storage
+    texture->bind();
+    GLuint format;
+    switch (channels)
+    {
+    case 1:
+        format = GL_R;
+        break;
+    
+    case 2:
+        format = GL_RG;
+        break;
 
+    case 3:
+       format=GL_RGB;
+        break;
+    case 4:
+       format=GL_RGBA;
+        break;
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, format, GL_UNSIGNED_BYTE, pixels);
+    if(generate_mipmap){
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
     stbi_image_free(pixels); //Free image data after uploading to GPU
     return texture;
 }
