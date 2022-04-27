b57a2449ae048b03c78a56b2680daf50c1cbe526
AndrewBoshra <andrew.boshr@gmail.com>
2022-04-27 15:49:30 +0200

    implemented entity

--- Phase2/source/common/components/camera.cpp
+++ Phase2/source/common/components/camera.cpp
@@ -35,7 +35,10 @@ namespace our {
         // - the center position which is the point (0,0,-1) but after being transformed by M
         // - the up direction which is the vector (0,1,0) but after being transformed by M
         // then you can use glm::lookAt
-        return glm::mat4(1.0f);
+        glm::vec3 eye = M * glm::vec4(0,0,0,1);
+        glm::vec3 center = M*glm::vec4(0,0,-1,1);
+        glm::vec3 up = M*glm::vec4(0,1,0,1);
+        return glm::lookAt(eye,center,up);
     }
 
     // Creates and returns the camera projection matrix
@@ -46,6 +49,20 @@ namespace our {
         // It takes left, right, bottom, top. Bottom is -orthoHeight/2 and Top is orthoHeight/2.
         // Left and Right are the same but after being multiplied by the aspect ratio
         // For the perspective camera, you can use glm::perspective
-        return glm::mat4(1.0f);
+
+
+        float aspectRatio=(float)viewportSize.x/viewportSize.y;
+        switch (cameraType)
+        {
+        case CameraType::PERSPECTIVE:
+            return glm::perspective(fovY, aspectRatio,near,far);
+        case CameraType::ORTHOGRAPHIC:
+            float top=orthoHeight/2;
+            float bottom=-1*top;
+            float left=bottom*aspectRatio;
+            float right=top*aspectRatio;
+            return glm::ortho(left,right,bottom,top);
+        }
+    return glm::mat4(1.0f);
     }
 }
